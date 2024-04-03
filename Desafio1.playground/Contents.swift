675b35d47bed06c12da95bb71f037678feb59dd0
Kaian Tiago <kaiantiago@outlook.com>
2024-04-03 11:41:46 -0300

    adicionando o desafio completo

--- Desafio1.playground/Contents.swift
+++ Desafio1.playground/Contents.swift
@@ -1,4 +1,10 @@
-let nome = "Steve"
-var sobrenome: String? = "Jobs"
+let name = "Steve"
+var surname: String? = "Jobs"
 
+if surname != nil {
+    print("This is an interpolation of \(name) and \(surname ?? "Wozniak")")
+}
 
+if var definiteSurname = surname {
+    print("This is an interpolation in an optionl binding of \(name) and \(definiteSurname)")
+}
